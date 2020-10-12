89a8498c9f229796ab31cffee5b7434d2a8e72ca
chirptotheeast <alyssa.easterly@yahoo.com>
2020-10-12 14:52:39 -0500

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,9 +1,13 @@
 ## code your solution here. 
+require 'pry'
 class Cat
-    attr_accessor :name
+   attr_accessor :name 
+
 
     def meow
-       @name = name
-        puts "meow!"
+      puts "meow!"
     end
 end
+
+
+
